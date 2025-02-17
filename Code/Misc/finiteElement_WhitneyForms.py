e578f094a3c52d78c725411efd2c9e594f4968d9
Nat Trask <45605841+natrask@users.noreply.github.com>
2025-02-17 09:14:18 -0500

    lecture material for today

--- Code/Misc/finiteElement_WhitneyForms.py
+++ Code/Misc/finiteElement_WhitneyForms.py
@@ -89,3 +89,61 @@ plt.plot(points.numpy(), u_sol.numpy())
 plt.title("Solution")
 plt.show()
 
+# %% Build up Whitney forms
+Nlevels = 3
+Npous = 2**(Nlevels)
+
+def binary_to_int(binary_str):
+    return int(binary_str, 2)
+
+def int_to_binary(integer, width):
+    return format(integer, f"0{width}b")
+
+print(int_to_binary(7, Nlevels))
+# %%
+
+class MLPWithSoftmax(nn.Module):
+    def __init__(self, input_dim, hidden_dim):
+        super(MLPWithSoftmax, self).__init__()
+        self.fc1 = nn.Linear(input_dim, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, hidden_dim)
+        self.fc3 = nn.Linear(hidden_dim, 2)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = x + F.relu(self.fc2(x))
+        x = self.fc3(x)
+        x = F.softmax(x,-1)  # Apply softmax along the last dimension
+        return x
+
+
+# Example usage
+input_dim = 1
+hidden_dim = 10
+model = MLPWithSoftmax(input_dim, hidden_dim)
+input_data = torch.randn(4, input_dim)  # Batch size of 4
+output_data = model(input_data)
+output_data.shape
+
+# Store indices across hierarchy levels
+indexLevels = [[2**l+z-1 for z in range(2**(l))] for l in range(Nlevels)]
+POUnets = [[MLPWithSoftmax(input_dim, hidden_dim)] for l in range(Nlevels)]
+
+
+# %%
+# Generate convex comb networks of same size as POUnets
+whitney_0s = [[0 for _ in sublist] for sublist in POUnets]
+whitney_0s[0][0] = POUnets[0][0]
+
+#W_i1,...,iN = \prod_{l=1}^{N} W_{i_l|i_l-1,...,i_1}
+for l in range(1,Nlevels):
+    for i in range(2**l):
+        print(f"Level {l}, index {i}")
+        parents = int_to_binary(i, l)[::-1]
+        print(parents)
+        W = POUnets[0][0]
+        for p in range(l):
+            print(p,parents[p])
+            W  = W*POUnets[p][parents[p]]
+        whitney_0s[l][i] = W
+# %%
