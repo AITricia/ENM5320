d70ee0bae7db724867179900f0c574fa2594f894
Nat Trask <nat.trask@gmail.com>
2025-03-30 14:03:11 -0400

    working hp

--- Code/finiteElement_hp.py
+++ Code/finiteElement_hp.py
@@ -23,7 +23,7 @@ print(f"Using device: {device}")
 
 # %%
 # Define points and h as needed
-meshsize= 24
+meshsize= 32
 h = 1./float(meshsize-1)
 points = torch.tensor(np.linspace(0,1,meshsize), dtype=torch.float64) 
 finepoints = torch.tensor(np.linspace(0,1,21*meshsize), dtype=torch.float64)  
@@ -37,7 +37,7 @@ def evalGradPhi_i(x):
     suppPhi = (evalPhi_i(x) > 0).double()
     signPlus = (torch.unsqueeze(points, 1) > torch.unsqueeze(x, 0)).double()
     signNeg = (torch.unsqueeze(points, 1) <= torch.unsqueeze(x, 0)).double()
-    return -suppPhi * (-signPlus + signNeg) / h
+    return suppPhi * (signPlus - signNeg) / h
 
 # Visualize shape functions and their derivatives evaluated over a fine grid
 phi_i = evalPhi_i(finepoints)
@@ -51,8 +51,18 @@ plt.plot(finepoints.numpy(), grad_phi_i.numpy().T)
 plt.title("Grad Phi_i")
 plt.show()
 
+# %% Build POU coarsening
+Npou = 5 #meshsize
+Wijlogit = torch.nn.Parameter(torch.randn(Npou, meshsize, dtype=torch.float64))
+Wijlogit.requires_grad = True
+# Wij = torch.nn.functional.softmax(Wijlogit+1e5*torch.eye(Npou), dim=0)
+Wij = torch.nn.functional.softmax(Wijlogit, dim=0)
+optimizer = optim.Adam([Wijlogit], lr=0.001)
+
+
 # %% Calculate Gauss-legendre weights and points of a given order
-Nquad = 4
+Nquad = 3
+polyOrder = 2
 qpts, qws = np.polynomial.legendre.leggauss(Nquad)  # Use NumPy to compute
 qpts = torch.tensor(qpts, dtype=torch.float64)
 qws = torch.tensor(qws, dtype=torch.float64)
@@ -62,19 +72,20 @@ indexsort = torch.argsort(xq)
 xq = xq[indexsort]
 qws = qws[indexsort]
 
-#Build POU coarsening
-Npou = meshsize
-polyOrder = 1
-Wijlogit = torch.nn.Parameter(torch.randn(Npou, meshsize, dtype=torch.float64))
-Wijlogit.requires_grad = True
-Wij = torch.nn.functional.softmax(Wijlogit+1e5*torch.eye(Npou), dim=0)
-# Wij = torch.nn.functional.softmax(Wijlogit, dim=0)
-
-optimizer = optim.Adam([Wijlogit], lr=0.01)
+N0forms = (polyOrder+1)*Npou
 
 # %% Train
-for i in range(100):
 
+for i in range(100000):
+    optimizer.zero_grad()
+    Wijbced = Wijlogit.clone()
+    Wijbced[0,:] = -1e10
+    Wijbced[-1,:] = -1e10
+    Wijbced[1:-1,0] = -1e10
+    Wijbced[1:-1,-1] = -1e10
+    Wijbced[0,0] += 1e10
+    Wijbced[-1,-1] += 1e10
+    Wij = torch.nn.functional.softmax(Wijbced, dim=0)
     # build pou basis and derivatives
     nodal_basisEval = evalPhi_i(xq)
     nodal_gradbasisEval = evalGradPhi_i(xq)
@@ -97,32 +108,38 @@ for i in range(100):
     # Construct discretization for Poisson with Dirichlet BCs on left and right
 
     # Set up forcing function evaluated on the nodes and specify dirichlet conditions
-    uexact = torch.cos(2.0*np.pi*xq)
-    dudxexact = -2.0*np.pi*torch.sin(2.0*np.pi*xq)
-    # uexact = xq**2
-    # dudxexact = 2.0*xq
-    forcing = -(4.0*np.pi**2)*torch.cos(2.0*np.pi*xq)
+    #pick random k betweren 1 and 3
+    # k = np.random.randint(1, 3)
+    k = 1
+    uexact = torch.cos(2.0*np.pi*k*xq)
+    dudxexact = -2.0*np.pi*k*torch.sin(2.0*np.pi*k*xq)
+    forcing = -(4.0*np.pi**2*k**2)*torch.cos(2.0*np.pi*k*xq)
     RHS = (h/2.)*torch.einsum('iq,q,q->i',psi0_i,forcing,qws)
+    uLHS = uexact[0]
+    uLHS2 = uexact[-1]
 
-    # for debugging, we can check that the L2 projection is correct
-    ones_quad = uexact
-    onehatRHS = (h/2.)*torch.einsum('iq,q,q->i',psi0_i,ones_quad,qws)
+    #  Build matrices
+    solution_rhs = torch.cat([uLHS.unsqueeze(0), RHS[1:-1], uLHS2.unsqueeze(0)], dim=0)
+    solution_mat = torch.cat([
+        torch.unsqueeze(torch.nn.functional.one_hot(torch.tensor(0), N0forms).double(), 0),
+        S0[1:-1, :],
+        torch.unsqueeze(torch.nn.functional.one_hot(torch.tensor(N0forms-1), N0forms).double(), 0),
+    ], dim=0)
+
 
     # Solve the linear system and plot solution
-    # u_sol = torch.linalg.lstsq(S0, RHS, rcond = 1e-14)[0]
-    u_sol = torch.linalg.lstsq(M0, onehatRHS, rcond = 1e-14)[0]
-
+    # u_sol = torch.linalg.lstsq(S0, RHS, rcond = 1e-9)[0]
+    u_sol = torch.linalg.solve(solution_mat, solution_rhs)
+    
     # expand solution in basis on the quadrature points and shift to match constant offset
     u_sol_expanded = torch.einsum('iq,i->q',psi0_i,u_sol)
     gradu_sol_expanded = torch.einsum('iq,i->q',gradpsi0_i,u_sol)
-
     u_sol_expanded = u_sol_expanded + uexact[0] - u_sol_expanded[0]
-
+    h1seminorm = torch.linalg.norm(gradu_sol_expanded - dudxexact, 2)
     # take gradient descent step to update wij
-    optimizer.zero_grad()
     error = u_sol_expanded - uexact
-    loss = torch.linalg.norm(error, 2)/torch.linalg.norm(uexact, 2)
-    print(f"Iteration {i+1}, Error: {loss.item()}")
+    loss = torch.linalg.norm(error, 2) + h1seminorm
+    print(f"Iteration {i+1}, Error: {loss.item()}, l2 norm: {torch.linalg.norm(error, 2).item()}, h1 seminorm: {h1seminorm.item()}")
     loss.backward(retain_graph=True)
     optimizer.step()
 
@@ -133,5 +150,9 @@ plt.plot(xq.numpy(), uexact.detach().numpy(), label='Solution')
 # %%
 plt.plot(xq.numpy(), gradu_sol_expanded.detach().numpy(), label='Solution')
 plt.plot(xq.numpy(), dudxexact.detach().numpy(), label='Solution')
+
+# %% visualize pous
+plt.plot(points.numpy(), Wij.detach().numpy().T)
+plt.title("POU basis functions")
 
 # %%
